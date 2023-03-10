65a522a5c13a2c16ef9744227fb6302787eb4971
bayu iman <senjuid@gmail.com>
2023-03-10 14:55:29 +0700

    update

---
+++ src/app/microfrontend-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class MicroFrontEndRoutingModule { }
---
+++ src/app/microfrontend.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MicroFrontEndRoutingModule } from './microfrontend-routing.module';
+
+@NgModule({
+  entryComponents: [],
+  imports: [
+    MicroFrontEndRoutingModule
+  ],
+  providers: [],
+})
+export class MicroFrontEndModule {}
